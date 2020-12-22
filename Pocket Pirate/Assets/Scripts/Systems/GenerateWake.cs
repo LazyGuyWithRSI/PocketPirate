2677a486550b4a11e9f9cd649c08ee9e0afcd6fa
Ian Chapman <gammaian@yahoo.com>
2020-12-22 12:07:54 -0800

    Update prefabs with new buoyancy and add better wake logic

---
+++ Pocket Pirate/Assets/Scripts/Systems/GenerateWake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWake : MonoBehaviour
+{
+    public Transform target;
+
+    public float XOffset = 0.0f;
+    public float ZOffset = 0.0f;
+
+    private ParticleSystem particleSys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSys = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("particle playing: " + particleSys.isEmitting);
+        if (target.transform.position.y > 0 && particleSys.isEmitting)
+        {
+            particleSys.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+        else if (target.transform.position.y <= 0 && !particleSys.isEmitting)
+        {
+            Debug.Log("playing wake");
+            particleSys.Play();
+        }
+
+        transform.position = new Vector3(target.position.x + XOffset, 0.3f, target.position.z + ZOffset);
+    }
+}
