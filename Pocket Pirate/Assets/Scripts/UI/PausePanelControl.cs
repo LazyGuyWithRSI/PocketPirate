9c3ce4007269b09c33f42810e88704bdbd6fec1a
Ian Chapman <gammaian@yahoo.com>
2021-01-04 16:09:47 -0800

    Bad pause menu implementation, will redo

--- Pocket Pirate/Assets/Scripts/UI/PausePanelControl.cs
+++ Pocket Pirate/Assets/Scripts/UI/PausePanelControl.cs
@@ -6,12 +6,15 @@ public class PausePanelControl : MonoBehaviour
 {
     Animator anim;
 
+    private const string BackBtnName = "BtnBack";
+
     // Start is called before the first frame update
     void Start ()
     {
         anim = GetComponent<Animator>();
         //gameObject.SetActive(false);
         PubSub.RegisterListener<OnPauseEvent>(OnPauseHandle);
+        PubSub.RegisterListener<OnButtonReleasedEvent>(OnButonReleasedHandle);
 
         // disable panel on start up, some focus issue means spacebar hits the try again button...
         gameObject.SetActive(false);
@@ -24,6 +27,15 @@ public class PausePanelControl : MonoBehaviour
         {
             gameObject.SetActive(true);
             anim.Play("PanelSlideUp");
+        }
+    }
+
+    public void OnButonReleasedHandle (object publishedEvent)
+    {
+        OnButtonReleasedEvent args = publishedEvent as OnButtonReleasedEvent;
+        if(args.Name.Equals(BackBtnName))
+        {
+            anim.Play("PanelSlideDown");
         }
     }
 
