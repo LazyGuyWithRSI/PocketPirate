eaa090612204eac4fdc4a6644e1d3f087653eaff
Ian Chapman <gammaian@yahoo.com>
2021-01-21 10:32:36 -0800

    Add proper wave spawning

---
+++ Pocket Pirate/Assets/Scriptable Objects/Spawning/SpawningWaveReference.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Reference/SpawningWave")]
+public class SpawningWaveReference : ScriptableObject
+{
+    public ThingToSpawn[] ThingsToSpawn;
+
+    [System.Serializable]
+    public class ThingToSpawn
+    {
+        public GameObject Prefab;
+        public float Weight = 1.0f;
+    }
+}
--- Pocket Pirate/Assets/Scripts/DebugEnemySpawner.cs
+++ Pocket Pirate/Assets/Scripts/DebugEnemySpawner.cs
@@ -5,18 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class DebugEnemySpawner : MonoBehaviour
 {
-    public GameObject EnemyPrefab;
-    public GameObject BigEnemyPrefab;
+    public SpawningWaveReference[] waves;
     public Vector3Reference PlayerPosition;
-    public int MaxEnemies = 5;
+    public int StartEnemies = 5;
     public float MaxSpawnDistance = 20f;
     public float MinSpawnDistance = 8f;
+    public float WaveTimeLimit = 10f;
 
-    public float Ramp = 0.5f;
+    public float Ramp = 1.4f;
 
     private int numEnemies;
-    private float elapsed;
-    private int extraEnemies = 0;
+    private int MaxEnemiesThisWave;
 
     private bool gameIsOver = false;
 
@@ -27,21 +26,24 @@ public class DebugEnemySpawner : MonoBehaviour
 
         numEnemies = 0;
 
+        /*
         for (int i = 0; i < MaxEnemies; i++)
         {
             SpawnEnemy();
         }
-
-        elapsed = 0f;
+        */
+        StartCoroutine(WaveControlCoroutine());
     }
 
     private void FixedUpdate ()
     {
+        /*
         elapsed += Time.fixedDeltaTime;
 
         extraEnemies = (int)(elapsed * Ramp);
         if (numEnemies < MaxEnemies + extraEnemies)
             SpawnEnemy();
+        */
     }
 
     public void OnGameOverEvent(object publishedEvent)
@@ -54,13 +56,15 @@ public class DebugEnemySpawner : MonoBehaviour
         OnDeathEvent args = publishedEvent as OnDeathEvent;
         if (args.Team == 1)
         {
+            /*
             numEnemies--;
             if (numEnemies < MaxEnemies + extraEnemies)
                 SpawnEnemy();
+            */
         }
     }
 
-    private void SpawnEnemy()
+    private void SpawnEnemy(int wave)
     {
         if (gameIsOver)
             return;
@@ -74,13 +78,53 @@ public class DebugEnemySpawner : MonoBehaviour
             xDelta = Random.Range(-MaxSpawnDistance, MaxSpawnDistance);
         }
 
+        // add up weights
+        
+        float totalWeight = 0;
+        for (int i = 0; i < waves[wave].ThingsToSpawn.Length; i++)
+            totalWeight += waves[wave].ThingsToSpawn[i].Weight;
+
+        float rand = Random.Range(0, totalWeight);
+        for (int i = 0; i < waves[wave].ThingsToSpawn.Length; i++)
+        {
+            if (rand < waves[wave].ThingsToSpawn[i].Weight)
+            {
+                Instantiate(waves[wave].ThingsToSpawn[i].Prefab, new Vector3(PlayerPosition.Value.x + xDelta, 1, PlayerPosition.Value.z + zDelta), Quaternion.identity);
+                break;
+            }
+
+            rand -= waves[wave].ThingsToSpawn[i].Weight;
+        }
+
+        /*
         if (Random.Range(0, 4) == 0)
             Instantiate(BigEnemyPrefab, new Vector3(PlayerPosition.Value.x + xDelta, 1, PlayerPosition.Value.z + zDelta), Quaternion.identity);
         else
             Instantiate(EnemyPrefab, new Vector3(PlayerPosition.Value.x + xDelta, 1, PlayerPosition.Value.z + zDelta), Quaternion.identity);
-
         numEnemies++;
+        */
     }
 
+    private IEnumerator WaveControlCoroutine()
+    {
+        int waveCount = 0;
+        StartCoroutine(SpawnWaveCoroutine(waveCount % waves.Length, StartEnemies, 1f));
+        while (!gameIsOver)
+        {
+            yield return new WaitForSeconds(WaveTimeLimit);
+            waveCount++;
+            StartCoroutine(SpawnWaveCoroutine(waveCount % waves.Length, (int)(StartEnemies * (Ramp * waveCount)), 1f));
+        }
+    }
 
+    private IEnumerator SpawnWaveCoroutine(int wave, int enemiesToSpawn, float interval)
+    {
+        while (enemiesToSpawn > 0)
+        {
+            yield return new WaitForSeconds(interval);
+
+            SpawnEnemy(wave);
+            enemiesToSpawn--;
+        }
+    }
 }
