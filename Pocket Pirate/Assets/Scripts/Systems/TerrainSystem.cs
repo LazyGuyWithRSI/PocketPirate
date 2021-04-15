95f9659466f55d9e0ff8560f0464866157513ace
Ian Chapman <gammaian@yahoo.com>
2021-04-14 18:47:14 -0700

    first pass at terrain generator / bug fixes

---
+++ Pocket Pirate/Assets/Scripts/Systems/TerrainSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSystem : MonoBehaviour
+{
+    public float noiseToUnitFactor = 10f;
+    public float noiseScale = 1f;
+    public float chanceToPlaceTerrain = 0.5f;
+    public int width = 100;
+    public int height = 100;
+
+    public GameObject terrainPrefab;
+
+    private float[,] noiseMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        noiseMap = NoiseGenerator.Generate(width, height, noiseScale, Vector2.zero);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (Random.Range(0f, 1f) < noiseMap[x, y] && Random.Range(0f, 1f) < chanceToPlaceTerrain)
+                    GameObject.Instantiate(terrainPrefab, new Vector3((x - (width / 2)) * noiseToUnitFactor, 0, (y - (height / 2)) * noiseToUnitFactor), Quaternion.Euler(0, Random.Range(0f, 360f), 0));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Pocket Pirate/Assets/Scripts/Util/NoiseGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator
+{
+    public static float[,] Generate (int width, int height, float scale, Vector2 offset)
+    {
+        float[,] noiseMap = new float[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float sampleX = (float)x * scale + offset.x;
+                float sampleY = (float)y * scale + offset.y;
+
+                noiseMap[x, y] = Mathf.PerlinNoise(sampleX, sampleY);
+            }
+        }
+
+
+        return noiseMap;
+    }
+}
