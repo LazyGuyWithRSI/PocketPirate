db256057b2ee7e82f826021cac16d017e94707e5
Ian Chapman <gammaian@yahoo.com>
2021-04-28 13:53:56 -0700

    Add sound toggle to settings

--- Pocket Pirate/Assets/Scriptable Objects/SettingsReference.cs
+++ Pocket Pirate/Assets/Scriptable Objects/SettingsReference.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class SettingsReference : ScriptableObject
 {
     public bool Use2AxisJoystick;
+    public bool SoundEnabled;
     public float MasterVolume;
     public float MusicVolume;
     public float EffectsVolume;
--- Pocket Pirate/Assets/Scripts/UI/SettingsPanelHandler.cs
+++ Pocket Pirate/Assets/Scripts/UI/SettingsPanelHandler.cs
@@ -7,15 +7,25 @@ public class SettingsPanelHandler : MonoBehaviour
 {
     public SettingsReference settings;
     public Toggle Use2AxisJoystickToggle;
+    public Toggle SoundToggle;
 
     void Start ()
     {
         Use2AxisJoystickToggle.isOn = settings.Use2AxisJoystick;
+        SoundToggle.isOn = settings.SoundEnabled;
+        AudioListener.volume = settings.SoundEnabled ? 1f : 0f; // TODO use audio manager
         Use2AxisJoystickToggle.onValueChanged.AddListener(OnToggle2AxisJoystick);
+        SoundToggle.onValueChanged.AddListener(OnToggleSound);
     }
 
     public void OnToggle2AxisJoystick(bool newVal)
     {
         settings.Use2AxisJoystick = newVal;
+    }
+
+    public void OnToggleSound(bool newVal)
+    {
+        settings.SoundEnabled = newVal;
+        AudioListener.volume = settings.SoundEnabled ? 1f : 0f;
     }
 }
