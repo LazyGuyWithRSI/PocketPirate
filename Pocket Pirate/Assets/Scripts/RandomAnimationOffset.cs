aea392ed455d51da9692f996d7570297c7602671
Ian Chapman <gammaian@yahoo.com>
2021-04-30 18:30:29 -0700

    new main menu scene

---
+++ Pocket Pirate/Assets/Scripts/RandomAnimationOffset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimationOffset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("start offset", Random.Range(0f, 1f));
+    }
+
+}
