5875e702a318f881db4bc2dca7d59c77aead942e
Ian Chapman <gammaian@yahoo.com>
2021-05-06 18:27:33 -0700

    Add json test

---
+++ Pocket Pirate/Assets/JsonTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class JsonTest : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    public FloatReference Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestClass testClass = JsonUtility.FromJson<TestClass>(jsonFile.text);
+
+        if (testClass.TestString.Equals("Hello there!"))
+            Score.Value = 666;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class TestClass
+{
+    public int ID;
+    public string TestString;
+}
