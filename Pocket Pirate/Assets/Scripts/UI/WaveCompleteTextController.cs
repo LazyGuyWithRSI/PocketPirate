0f1a71baa18b30723bb6f39c5d3393409a4b6ae9
Ian Chapman <gammaian@yahoo.com>
2021-05-07 16:12:14 -0700

    Add wave start/end text

---
+++ Pocket Pirate/Assets/Scripts/UI/WaveCompleteTextController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WaveCompleteTextController : MonoBehaviour
+{
+    public float ShowDuration = 2f;
+    public TMP_Text text;
+    public FloatReference CurrentWave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PubSub.RegisterListener<OnWaveOver>(OnWaveOverHandler);
+        StartCoroutine(ShowTextCoroutine(ShowDuration, ""));
+    }
+
+    private void OnWaveOverHandler(object publishedObject)
+    {
+        StartCoroutine(ShowTextCoroutine(ShowDuration, "  Complete"));
+    }
+
+    private IEnumerator ShowTextCoroutine(float duration, string postText)
+    {
+        text.text = "Wave  " + CurrentWave.Value + postText;
+        yield return new WaitForSeconds(duration);
+        text.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
